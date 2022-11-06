1cd84e32ead4501f3b32c290df30e08adae6b32b
selfkilla666 <selfkilla666@gmail.com>
2022-11-06 19:21:54 +0200

    Add files via upload

---
+++ donatello/exceptions.py
@@ -0,0 +1,12 @@
+
+class NoTokenError(Exception):
+    pass
+
+class AuthenticateError(Exception):
+    pass
+
+class IncompleteProfileSettings(Exception):
+    pass
+
+class AsyncNotWorkingNow(Exception):
+    pass
